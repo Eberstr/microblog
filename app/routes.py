f274c0f2a440074b10edef3db0055436c2150bf4
eber-antonio <eber.antonio9298@gmail.com>
2024-12-15 17:23:34 -0600

    HTML templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world!"
+    user = {"username":"Eber"}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in portlan!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Hi post from susan'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
